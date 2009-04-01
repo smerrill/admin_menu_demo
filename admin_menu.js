c4497016715971a201504a5475e354c8eb3002cf
sun <sun@unleashedmind.com>
2009-04-01 17:42:18 +0000

    #345984 by markus_petrux, sun: Added client-side caching of administration menu.   Attention: A new era of Drupal user experience starts here.  This is the very   first issue of a series of improvements targeting plain awesomeness.

--- admin_menu.js
+++ admin_menu.js
@@ -10,18 +10,40 @@ Drupal.admin = Drupal.admin || { behaviors: {} };
  * registered behaviors.
  */
 Drupal.behaviors.adminMenu = {
-  attach: function (context) {
+  attach: function (context, settings) {
     // Initialize settings.
-    Drupal.settings.admin_menu = $.extend({
+    settings.admin_menu = $.extend({
+      suppress: false,
       margin_top: false,
       position_fixed: false,
       tweak_modules: false,
       tweak_tabs: false,
-      destination: ''
-    }, Drupal.settings.admin_menu || {});
+      destination: '',
+      basePath: settings.basePath,
+      hash: 0
+    }, settings.admin_menu || {});
+    // Check whether administration menu should be suppressed.
+    if (settings.admin_menu.suppress) {
+      return;
+    }
     var $adminMenu = $('#admin-menu:not(.admin-menu-processed)', context);
-    // Apply our behaviors.
-    Drupal.admin.attachBehaviors(context, $adminMenu);
+    // Client-side caching; if administration menu is not in the output, it is
+    // fetched from the server and cached in the browser.
+    if (!$adminMenu.length && settings.admin_menu.hash) {
+      Drupal.admin.getCache(settings.admin_menu.hash, function (response) {
+          if (typeof response == 'string' && response.length > 0) {
+            $('body', context).prepend(response);
+          }
+          var $adminMenu = $('#admin-menu:not(.admin-menu-processed)', context);
+          // Apply our behaviors.
+          Drupal.admin.attachBehaviors(context, settings, $adminMenu);
+      });
+    }
+    // If the menu is in the output already, this means there is a new version.
+    else {
+      // Apply our behaviors.
+      Drupal.admin.attachBehaviors(context, settings, $adminMenu);
+    }
   }
 };
 
@@ -31,8 +53,8 @@ Drupal.behaviors.adminMenu = {
  * For why multiple selectors see #111719.
  */
 Drupal.behaviors.adminMenuCollapseModules = {
-  attach: function (context) {
-    if (Drupal.settings.admin_menu.tweak_modules) {
+  attach: function (context, settings) {
+    if (settings.admin_menu.tweak_modules) {
       $('#system-modules fieldset:not(.collapsed), #system-modules-1 fieldset:not(.collapsed)', context).addClass('collapsed');
     }
   }
@@ -45,13 +67,32 @@ Drupal.behaviors.adminMenuCollapseModules = {
  * flickering/jumping page content with client-side caching, this is a regular
  * Drupal behavior.
  */
-Drupal.behaviors.adminMenuMarginTop = function (context) {
-  attach: function (context) {
-    if (Drupal.settings.admin_menu.margin_top) {
+Drupal.behaviors.adminMenuMarginTop = {
+  attach: function (context, settings) {
+    if (settings.admin_menu.margin_top) {
       $('body', context).addClass('admin-menu');
     }
   }
 };
+
+/**
+ * Retrieve content from client-side cache.
+ *
+ * @param hash
+ *   The md5 hash of the content to retrieve.
+ * @param onSuccess
+ *   A callback function invoked when the cache request was successful.
+ */
+Drupal.admin.getCache = function (hash, onSuccess) {
+  $.ajax({
+    cache: true,
+    type: 'GET',
+    dataType: 'text', // Prevent auto-evaluation of response.
+    global: false, // Do not trigger global AJAX events.
+    url: Drupal.settings.admin_menu.basePath + 'js/admin_menu/cache/' + hash,
+    success: onSuccess
+  });
+}
 
 /**
  * @defgroup admin_behaviors Administration behaviors.
@@ -61,11 +102,11 @@ Drupal.behaviors.adminMenuMarginTop = function (context) {
 /**
  * Attach administrative behaviors.
  */
-Drupal.admin.attachBehaviors = function (context, $adminMenu) {
+Drupal.admin.attachBehaviors = function (context, settings, $adminMenu) {
   if ($adminMenu.length) {
     $adminMenu.addClass('admin-menu-processed');
     $.each(Drupal.admin.behaviors, function() {
-      this(context, $adminMenu);
+      this(context, settings, $adminMenu);
     });
   }
 };
@@ -73,8 +114,8 @@ Drupal.admin.attachBehaviors = function (context, $adminMenu) {
 /**
  * Apply 'position: fixed'.
  */
-Drupal.admin.behaviors.positionFixed = function (context, $adminMenu) {
-  if (Drupal.settings.admin_menu.position_fixed) {
+Drupal.admin.behaviors.positionFixed = function (context, settings, $adminMenu) {
+  if (settings.admin_menu.position_fixed) {
     $adminMenu.css('position', 'fixed');
   }
 };
@@ -82,8 +123,8 @@ Drupal.admin.behaviors.positionFixed = function (context, $adminMenu) {
 /**
  * Move page tabs into administration menu.
  */
-Drupal.admin.behaviors.pageTabs = function (context, $adminMenu) {
-  if (Drupal.settings.admin_menu.tweak_tabs) {
+Drupal.admin.behaviors.pageTabs = function (context, settings, $adminMenu) {
+  if (settings.admin_menu.tweak_tabs) {
     $('ul.tabs.primary li', context).addClass('admin-menu-tab').appendTo('#admin-menu > ul');
     $('ul.tabs.secondary', context).appendTo('#admin-menu > ul > li.admin-menu-tab.active');
     $('ul.tabs.primary', context).remove();
@@ -93,8 +134,8 @@ Drupal.admin.behaviors.pageTabs = function (context, $adminMenu) {
 /**
  * Inject destination query strings for current page.
  */
-Drupal.admin.behaviors.destination = function (context, $adminMenu) {
-  if (Drupal.settings.admin_menu.destination) {
+Drupal.admin.behaviors.destination = function (context, settings, $adminMenu) {
+  if (settings.admin_menu.destination) {
     $('.admin-menu-destination', $adminMenu).each(function() {
       this.search += (!this.search.length ? '?' : '&') + Drupal.settings.admin_menu.destination;
     });
@@ -107,7 +148,7 @@ Drupal.admin.behaviors.destination = function (context, $adminMenu) {
  * @todo This has to run last.  If another script registers additional behaviors
  *   it will not run last.
  */
-Drupal.admin.behaviors.hover = function (context, $adminMenu) {
+Drupal.admin.behaviors.hover = function (context, settings, $adminMenu) {
   // Hover emulation for IE 6.
   if ($.browser.msie && parseInt(jQuery.browser.version) == 6) {
     $('li', $adminMenu).hover(function() {
