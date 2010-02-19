3b57408d46da3907cffe64a4be03a89119b26875
sun <sun@unleashedmind.com>
2010-02-19 23:19:00 +0000

    by sun: Fixed various styling issues for admin_menu and admin_menu_toolbar.

--- admin_menu_toolbar/admin_menu_toolbar.js
+++ admin_menu_toolbar/admin_menu_toolbar.js
@@ -10,20 +10,13 @@ Drupal.admin.behaviors = Drupal.admin.behaviors || {};
  */
 
 /**
- * Add neat shadow.
- */
-Drupal.admin.behaviors.toolbarShadow = function (context, settings, $adminMenu) {
-  $('#admin-menu-wrapper', $adminMenu).append('<div id="admin-menu-shadow"></div>');
-};
-
-/**
  * Apply active trail highlighting based on current path.
  *
  * @todo Not limited to toolbar; move into core?
  */
 Drupal.admin.behaviors.toolbarActiveTrail = function (context, settings, $adminMenu) {
   if (settings.admin_menu.toolbar && settings.admin_menu.toolbar.activeTrail) {
-    $adminMenu.find('div > ul > li > a[href=' + settings.admin_menu.toolbar.activeTrail + ']').addClass('active-trail');
+    $adminMenu.find('> div > ul > li > a[href=' + settings.admin_menu.toolbar.activeTrail + ']').addClass('active-trail');
   }
 };
 
